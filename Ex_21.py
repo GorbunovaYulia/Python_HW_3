e73a4457c67c8d3ab3ebf5f69f4e48577ff68ea1
Yulia_Gorbunova <yuliagorbunova@yahoo.com>
2023-03-31 23:53:45 +0500

    EX_21

--- Ex_21.py
+++ Ex_21.py
@@ -5,6 +5,10 @@ Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
 ":" S007 "}]
 Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
 '''
-sl = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},{"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII":" S007 "}]
-for i in sl:
-    print(i)
+list_1 = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},{"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII":" S007 "}]
+sp = set()
+for dict_i in list_1:
+    for key in dict_i:
+        # print(dict_i[key].strip())
+        sp.add(dict_i[key].strip())
+print(sp)
